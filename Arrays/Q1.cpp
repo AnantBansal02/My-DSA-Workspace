e5738885a4e610108f9ff38a544c5e380c2b7dae
Anant <bansalanant206@gmail.com>
2022-02-13 12:51:03 +0530

    Initial commit

--- Arrays/Q1.cpp
+++ Arrays/Q1.cpp
@@ -1,68 +1,15 @@
-// Nested for loop {increased time complexity!!!}
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// int main()
-// {
-//     int n;
-//     cin>>n;
-//     int a[n];
-//     for(int i=0;i<n;i++)
-//     {
-//         cin>>a[i];
-//     }
-//     int ans = INT_MAX;
-//     for(int i=0;i<n;i++)
-//     {
-//         for(int j=0;j<i;j++)
-//         {
-//             if ( a[i] == a[j] && j!=0){ans = min(ans,j);}
-
-//         }
-//     }
-//     if(ans == INT_MAX){cout<<"No repitition!!!";}
-//     if(ans != INT_MAX){cout<<"Smallest Index of repeating unit = "<<ans+1<<endl;}
-// }
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int n;
-    cin>>n;
-    int mx = INT_MIN;
     int a[n];
+    cin>>n;
     for(int i=0;i<n;i++)
     {
         cin>>a[i];
-        mx = max(mx,a[i]);
-    }
-    int idx[mx];
-    for(int i=0;i<mx;i++)
-    {
-        idx[i] = -1;
-    }
-    int minidx = INT_MAX;
-    for(int i=0 ; i<n ;i++)
-    {
-        if(idx[a[i]] != -1)
-        {
-            minidx = min (minidx, idx[a[i]]);
-        }       
-        else
-        {
-            idx[a[i]] = i;
-        }
-    }
-    if(minidx == INT_MAX)
-    {
-        cout<<"-1"<<endl;
-    }
-    else
-    {
-        cout<<minidx+1<<endl;
     }
 
-    
     return 0;
 }
--- IIIT Seniors/abc.c
+++ IIIT Seniors/abc.c
@@ -1,11 +1,67 @@
-#include <stdio.h>
-int main()
-{
+// #include <stdio.h>
+// int main()
+// {
+//     int a,b;
+//     printf("Input Two Numbers!");
+//     scanf("%d",&a);
+//     scanf("%d",&b);
+//     int c=a+b;
+//     printf("Addition Sum = %d",c);
+//     return 0;
+// }
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main(){
+    string s="vadodara";
+    string ros;
+    
+    for(int i=0;i<s.size();i+=3){
+        
+    
+       
+       if((i+2)==s.size()){
+          ros=ros+s[i+1]+s[i];
+       }
+       else{
+            ros=ros+s[i+2]+s[i+1]+s[i];
+       }
+    
+   
+       
+    }
+    char arr[26];
+    arr[0]='a';
+    int i;
+    for( i=1;i<26;i++){
+        arr[i]='a'+i;
+
+    }
+    string ros2;
+    for(int w=0;w<ros.size();w++){
+      for(int r=0;r<26;r++){
+        if(ros[w]==arr[r]){
+            ros2[w]=arr[25-r];
+        }
+    }  
+    }
+    int count=0;
     int a,b;
-    printf("Input Two Numbers!");
-    scanf("%d",&a);
-    scanf("%d",&b);
-    int c=a+b;
-    printf("Addition Sum = %d",c);
+   cin>>a>>b;
+   int c,d;
+   while(d>c){
+       for(int u=0;u<26;u++){
+           for(c=a;c<=b;c++){
+           if(ros2[c]==arr[u]{
+               if(u>)
+           })
+       }
+       
+       }
+   }
+    
+    
     return 0;
 }
