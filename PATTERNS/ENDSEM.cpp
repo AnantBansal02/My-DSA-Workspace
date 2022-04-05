645b76300310c8977d75461dd26814792aff4edf
Anant <bansalanant206@gmail.com>
2022-04-05 21:09:37 +0530

    Initial commit

---
+++ PATTERNS/ENDSEM.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d",&N)
+    int pattern[N][N];
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            if(i<=1 || j==0 || j==i){pattern[i][j] = 1;}
+            else{pattern[i][j] = pattern[i-1][j] + pattern[i-1][j-1];}
+        }
+    }
+
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            printf("%d ",pattern[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
